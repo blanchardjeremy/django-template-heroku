8f84258fa0ee7ce1ea8da75607ba4001824336c2
Jeremy Blanchard <auzigog@gmail.com>
2012-03-25 18:09:58 -0700

    Add middleware for 1.4 xframeoptions

--- root/settings.py
+++ root/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
