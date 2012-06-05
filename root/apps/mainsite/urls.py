20bee7e3758e6d967c7bc255cacf71bfb33e85b4
Jeremy Blanchard <auzigog@gmail.com>
2012-06-05 10:39:47 -0700

    Fix urls for home page and serving static

--- root/apps/mainsite/urls.py
+++ root/apps/mainsite/urls.py
@@ -11,13 +11,14 @@ handler404 = 'mainsite.views.error404'
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^test/$', TestView.as_view(), name='test'),
+
+    url(r'^$', HomeView.as_view(), name='home'),
 
     (r'', include('SAMPLEAPP.urls')),
 )
 
 
-if not settings.DEBUG or settings.DJANGO_SERVE_STATIC:
+if settings.DJANGO_SERVE_STATIC:
     urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
