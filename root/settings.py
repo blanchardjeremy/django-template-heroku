508596c72b46d435006cd4a650f2c0dfdb9c1ada
Jeremy Blanchard <auzigog@gmail.com>
2012-06-05 20:28:52 -0700

    Correct dj database url in settings

--- root/settings.py
+++ root/settings.py
@@ -127,19 +127,8 @@ MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 #  Database
 #
 ##
-
-uses_netloc.append('postgres')
-url = urlparse(os.environ['DATABASE_URL'])
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': url.path[1:],
-        'USER': url.username,
-        'PASSWORD': url.password,
-        'HOST': url.hostname,
-        'PORT': url.port,
-    }
-}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 
 ##
