6ef557c15caacd59d5545ea4a20f1e6b482c7c30
Jeremy Blanchard <auzigog@gmail.com>
2012-06-14 14:07:10 -0700

    Convert 404 and 500 handlers to class based views

--- root/apps/mainsite/urls.py
+++ root/apps/mainsite/urls.py
@@ -5,16 +5,13 @@ from mainsite.views import *
 
 admin.autodiscover()
 
-handler500 = 'mainsite.views.error500'
-handler404 = 'mainsite.views.error404'
-
 urlpatterns = patterns('',
-
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^$', HomeView.as_view(), name='home'),
 
-    (r'', include('SAMPLEAPP.urls')),
+    # Test URLs to allow you to see these pages while DEBUG is True
+    url(r'^error/404/$', Error404.as_view(), name='404'),
+    url(r'^error/500/$', Error500.as_view(), name='500'),
 )
 
 
