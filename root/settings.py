fd0d8f407a211be01e8c918be0ad31f0ad7bf52e
Jeremy Blanchard <auzigog@gmail.com>
2013-03-24 15:18:04 -0700

    Upgrade requirements to django 1.5

--- root/settings.py
+++ root/settings.py
@@ -127,6 +127,7 @@ MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 #  Database
 #
 ##
+
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
@@ -189,6 +190,9 @@ USE_TZ = True
 
 TIME_ZONE = 'America/Los_Angeles'
 LANGUAGE_CODE = 'en-us'
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
